336ddf435d7014176ab3c9645f864a2a234f9c2a
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-23 09:08:55 +0200

    added tsv format

---
+++ code/com/goalpioneers/modules/formatter/flatfiles/tsv/TSVFilesReader.java
@@ -0,0 +1,13 @@
+package com.goalpioneers.modules.formatter.flatfiles.tsv;
+
+import com.goalpioneers.modules.formatter.flatfiles.FlatFilesReader;
+
+
+/**
+ * 
+ */
+public class TSVFilesReader 
+	extends FlatFilesReader
+{
+	
+}
---
+++ code/com/goalpioneers/modules/formatter/flatfiles/tsv/TSVFilesWriter.java
@@ -0,0 +1,13 @@
+package com.goalpioneers.modules.formatter.flatfiles.tsv;
+
+import com.goalpioneers.modules.formatter.flatfiles.FlatFilesWriter;
+
+
+/**
+ * 
+ */
+public class TSVFilesWriter 
+	extends FlatFilesWriter
+{
+	
+}
